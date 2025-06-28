0c070db9b61d1eea1f681d5f9d30560f6d68afd4
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-06-28 15:39:35 +0530

    Add files via upload

---
+++ loops.R
@@ -0,0 +1,13 @@
+cat("Enter number of times you want to loop :")
+n=scan(what=numeric(),nmax=1)
+for(i in 1:n){
+  cat("R Programming \n")
+}
+# like in pythin for i in range of n is written as above i.e till n i runs
+
+# in while loop we have to give condition 
+j<-1
+while(j<=5){
+  cat("R Programming \n")
+  j<-j+1
+}
