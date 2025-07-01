c78f880630ec76ef42b52380c9360060c7200b49
Farhan-Ahmed-Mohammed <farhan.ou888@gmail.com>
2025-07-01 21:29:25 +0530

    Update line_graph.R

--- line_graph.R
+++ line_graph.R
@@ -2,5 +2,13 @@ meantemp<-mean(airquality$Temp)
 cat("The mean of temp is :",meantemp)
 sdtemp<-sd(airquality$Temp)
 cat("The standard deviation of temp is ",sdtemp)
-plot(airquality$Temp,airquality$Wind,type="l",xlab="Temperature",ylab="Wind",main="Line graph between temp and wind")
+
+cleaned_data<-na.omit(airquality[,c("Temp","Wind")])
+# here in data to remove missing values we use na.omit and airquality[,c()] is actually dataframe[row,column] if nothing before first , then select all the rows
+
+sorted_data<-cleaned_data[order(airquality$Temp),]
+#in cleaned data only 2 columns are there
+
+plot(sorted_data$Temp,sorted_data$Wind,type="l",xlab="Temperature",ylab="Wind",main="Line graph between temp and wind")
 # here xlab means x lable and ylab means y label and type l means linear graph
+
